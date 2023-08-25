a90bd03ddabcfa2b1459cc66716e081aae5cb32f
codeboychik <andrey.chernenko1101@gmail.com>
2023-08-25 16:03:34 +0300

    web version of todo app: layout, add_task, remove_task

--- web.py
+++ web.py
@@ -3,21 +3,20 @@ import modules
 
 
 def add_task():
-    modules.add_task(st.session_state['new_task'])
-    st.write(st.session_state)
-
-
-def remove_task():
-    return None
-    # st.write(st.session_state)
+    if len(st.session_state['new_task']) > 0:
+        modules.add_task(st.session_state['new_task'])
+    st.session_state['new_task'] = ''
 
 
 st.title('My todo app')
 st.text(body=f'Last update: {modules.get_human_readable_time(modules.get_dictionary()["last_update"])}')
 
 for task in modules.get_task_list():
-    st.checkbox(label=task, value=False, key=id(task), on_change=remove_task())
-    print(id(task))
+    ch = st.checkbox(label=task, key=task)
+    if ch:
+        modules.remove_task(task)
+        del st.session_state[task]
+        st.experimental_rerun()
 
 
 st.text_input(placeholder='Enter a task: ', key='new_task', on_change=add_task, label='')
